42bb7df09e37bd0f3f7a80a800ffc1364cc0508e
pauel <pau.lacazedieu@gmail.com>
2026-05-05 10:51:46 +0200

    Corrected the fix with Squeegy's feedback. ( see https://discord.com/channels/909034158205059082/1494103095049982145 on NO official discord)

--- NOLoftFix/Patches/TopAttackFix.cs
+++ NOLoftFix/Patches/TopAttackFix.cs
@@ -10,45 +10,36 @@ namespace NOLF.Patches;
 public class MissileFixes
 {
 
-    public static float computeTTT(OpticalSeeker seeker)
+    public static void computeTTT(OpticalSeeker seeker)
     {
-        Vector3 vector3 = (seeker.knownPos - seeker.missile.GlobalPosition()) with
+        var hdir = (seeker.knownPos - seeker.missile.GlobalPosition()) with
         {
             y = 0.0f
         };
-        return seeker.targetDist / Mathf.Max(Vector3.Dot(vector3.normalized, seeker.missile.rb.velocity), seeker.selfDestructAtSpeed);
+        seeker.targetDist = hdir.magnitude;
+        var hvel = Vector3.Dot(hdir.normalized, seeker.missile.rb.velocity);
+        var spd = Mathf.Max(hvel, seeker.selfDestructAtSpeed);
+        var result = seeker.targetDist / spd;
+        seeker.timeToTarget = result;
     }
 
     [HarmonyPatch(nameof(OpticalSeeker.Initialize))]
     [HarmonyTranspiler]
     public static IEnumerable<CodeInstruction> InitializeTranspiler(IEnumerable<CodeInstruction> instructions)
     {
-        var tttField = AccessTools.Field(typeof(OpticalSeeker), nameof(OpticalSeeker.timeToTarget));
+        var missileNetworkSeekerModeSetter =
+            AccessTools.DeclaredPropertySetter(typeof(Missile), nameof(Missile.NetworkseekerMode));
         var tttComputeMethod = AccessTools.Method(typeof(MissileFixes), nameof(computeTTT));
         
         CodeInstruction? prev = null;
         foreach (var instr in instructions)
         {
-            if (prev is null)
-            {
-                prev = instr;
-                continue;
-            }
-
-            if (prev.opcode == OpCodes.Ldc_R4 &&
-                Mathf.Approximately((float)prev.operand, 100f) &&
-                instr.opcode == OpCodes.Stfld &&
-                (FieldInfo)instr.operand == tttField)
+            yield return instr;
+            if (instr.Calls(missileNetworkSeekerModeSetter))
             {
                 yield return  new CodeInstruction(OpCodes.Ldarg_0);
                 yield return  new CodeInstruction(OpCodes.Call, tttComputeMethod);
-            } else
-            {
-                yield return prev;
-            }        
-            prev = instr;
+            }
         }
-        if (prev is not null)
-            yield return prev;
     }
 }
